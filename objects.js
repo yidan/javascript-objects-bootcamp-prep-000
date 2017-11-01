20d56bffc476bce26289e23abb8a630397a0c266
Yidan Wang <wangyidan621@gmail.com>
2017-11-01 17:46:50 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -7,3 +7,8 @@ function udpatePlaylist(playlist, name, song){
  playlist.name = song;
  return playlist;
 }
+
+function removeFromPlaylist(playlist, name){
+  delete playlist.name
+  return playlist
+}
